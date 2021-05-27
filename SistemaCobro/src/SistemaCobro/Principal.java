dc70cc9ee370777a458e807a9bae60585516427c
Victoria R Erre Eme <rake.vrrm@gmail.com>
2021-05-26 21:46:41 -0500

    Interfaz Principal y Pagos

--- SistemaCobro/src/SistemaCobro/Principal.java
+++ SistemaCobro/src/SistemaCobro/Principal.java
@@ -8,20 +8,26 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 public class Principal extends javax.swing.JFrame {
 
     DecimalFormat df = new DecimalFormat("#.00000000");
     static int contador = 0;
-
+    
+    
     public Principal() {
         initComponents();
         this.setLocationRelativeTo(null);
         Panel_fondo_menu.setVisible(false);
-Panel_Menu_Finanzas.setVisible(false);
+        Panel_Menu_Finanzas.setVisible(false);
         Panel_menu.setVisible(false);
-    
-       
+        
+    }
+    public Image getIconImage (){
+        Image retValue = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("imagenes2/logo.jpg"));
+        return retValue;
     }
 
     @SuppressWarnings("unchecked")
@@ -29,6 +35,8 @@ Panel_Menu_Finanzas.setVisible(false);
     private void initComponents() {
 
         Panel_principal = new javax.swing.JPanel();
+        btnExit = new javax.swing.JButton();
+        btnMin = new javax.swing.JButton();
         btnmenu = new javax.swing.JButton();
         Panel_Menu_Finanzas = new javax.swing.JPanel();
         jButton2 = new javax.swing.JButton();
@@ -42,10 +50,32 @@ Panel_Menu_Finanzas.setVisible(false);
         lblLogo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setIconImage(getIconImage());
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         Panel_principal.setBackground(new java.awt.Color(10, 31, 34));
         Panel_principal.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnExit.setBackground(new java.awt.Color(10, 31, 34));
+        btnExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes2/icons8_cancel_40px.png"))); // NOI18N
+        btnExit.setBorder(null);
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
+        Panel_principal.add(btnExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 10, -1, -1));
+
+        btnMin.setBackground(new java.awt.Color(10, 31, 34));
+        btnMin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes2/icons8_minus_40px.png"))); // NOI18N
+        btnMin.setBorder(null);
+        btnMin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMinActionPerformed(evt);
+            }
+        });
+        Panel_principal.add(btnMin, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 10, -1, -1));
 
         btnmenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/btn_menu_sin_presionar.png"))); // NOI18N
         btnmenu.setBorderPainted(false);
@@ -60,7 +90,12 @@ Panel_Menu_Finanzas.setVisible(false);
                 btnmenuMouseExited(evt);
             }
         });
-        Panel_principal.add(btnmenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(17, 10, 70, 70));
+        btnmenu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnmenuActionPerformed(evt);
+            }
+        });
+        Panel_principal.add(btnmenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 20, 70, 70));
 
         Panel_Menu_Finanzas.setBackground(new java.awt.Color(255, 255, 255));
         Panel_Menu_Finanzas.setOpaque(false);
@@ -74,6 +109,9 @@ Panel_Menu_Finanzas.setVisible(false);
         });
         Panel_Menu_Finanzas.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jButton2.setBackground(new java.awt.Color(10, 31, 34));
+        jButton2.setFont(new java.awt.Font("Segoe UI Semilight", 0, 14)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
         jButton2.setText("Registrar Deuda");
         jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -88,8 +126,11 @@ Panel_Menu_Finanzas.setVisible(false);
                 jButton2ActionPerformed(evt);
             }
         });
-        Panel_Menu_Finanzas.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, 140, 40));
+        Panel_Menu_Finanzas.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, 140, 40));
 
+        jButton1.setBackground(new java.awt.Color(10, 31, 34));
+        jButton1.setFont(new java.awt.Font("Segoe UI Semilight", 0, 14)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setText("Registrar Pago");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -104,7 +145,7 @@ Panel_Menu_Finanzas.setVisible(false);
                 jButton1ActionPerformed(evt);
             }
         });
-        Panel_Menu_Finanzas.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, 140, 40));
+        Panel_Menu_Finanzas.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 60, 140, 40));
 
         Panel_principal.add(Panel_Menu_Finanzas, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 90, 210, -1));
 
@@ -121,6 +162,7 @@ Panel_Menu_Finanzas.setVisible(false);
         Panel_menu.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         btn1.setBackground(new java.awt.Color(37, 66, 70));
+        btn1.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
         btn1.setForeground(new java.awt.Color(240, 240, 240));
         btn1.setText("Finanzas");
         btn1.setToolTipText("");
@@ -139,9 +181,10 @@ Panel_Menu_Finanzas.setVisible(false);
                 btn1ActionPerformed(evt);
             }
         });
-        Panel_menu.add(btn1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 100, 140, 70));
+        Panel_menu.add(btn1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 100, 145, 70));
 
         btn3.setBackground(new java.awt.Color(37, 66, 70));
+        btn3.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
         btn3.setForeground(new java.awt.Color(240, 240, 240));
         btn3.setText("Consultar");
         btn3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(240, 240, 240), new java.awt.Color(240, 240, 240), new java.awt.Color(51, 51, 51), new java.awt.Color(102, 102, 102)));
@@ -159,11 +202,12 @@ Panel_Menu_Finanzas.setVisible(false);
                 btn3ActionPerformed(evt);
             }
         });
-        Panel_menu.add(btn3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 260, 140, 70));
+        Panel_menu.add(btn3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 145, 70));
 
         btn2.setBackground(new java.awt.Color(37, 66, 70));
+        btn2.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
         btn2.setForeground(new java.awt.Color(240, 240, 240));
-        btn2.setText("Registrar alumno");
+        btn2.setText("Registrar Alumno");
         btn2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(240, 240, 240), new java.awt.Color(240, 240, 240), new java.awt.Color(51, 51, 51), new java.awt.Color(102, 102, 102)));
         btn2.setBorderPainted(false);
         btn2.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -179,11 +223,12 @@ Panel_Menu_Finanzas.setVisible(false);
                 btn2ActionPerformed(evt);
             }
         });
-        Panel_menu.add(btn2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 140, 70));
+        Panel_menu.add(btn2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, 145, 70));
 
         btn4.setBackground(new java.awt.Color(37, 66, 70));
+        btn4.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
         btn4.setForeground(new java.awt.Color(240, 240, 240));
-        btn4.setText("Registrar usuario");
+        btn4.setText("Registrar Usuario");
         btn4.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(240, 240, 240), new java.awt.Color(240, 240, 240), new java.awt.Color(51, 51, 51), new java.awt.Color(102, 102, 102)));
         btn4.setBorderPainted(false);
         btn4.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -199,9 +244,9 @@ Panel_Menu_Finanzas.setVisible(false);
                 btn4ActionPerformed(evt);
             }
         });
-        Panel_menu.add(btn4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 343, 140, 70));
+        Panel_menu.add(btn4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 340, 145, 70));
 
-        Panel_principal.add(Panel_menu, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 150, 570));
+        Panel_principal.add(Panel_menu, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 170, 570));
 
         Panel_fondo_menu.setBackground(new java.awt.Color(121, 162, 167));
         Panel_fondo_menu.setForeground(new java.awt.Color(121, 162, 167));
@@ -226,7 +271,6 @@ Panel_Menu_Finanzas.setVisible(false);
         // TODO add your handling code here:
         Panel_Menu_Finanzas.setVisible(true);
         Panel_menu.setVisible(true);
-
         Panel_fondo_menu.setVisible(true);
     }//GEN-LAST:event_btn1MouseEntered
 
@@ -334,6 +378,7 @@ Panel_Menu_Finanzas.setVisible(false);
     }//GEN-LAST:event_btn4ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.setExtendedState(ICONIFIED);
         RegistrarPago regpag = new RegistrarPago();
         regpag.setTitle("Registrar Pago");
         regpag.setLocationRelativeTo(null);
@@ -371,11 +416,25 @@ Panel_Menu_Finanzas.setVisible(false);
     }//GEN-LAST:event_jButton1MouseExited
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.setExtendedState(ICONIFIED);
         RegistrarDeuda vdeuda = new RegistrarDeuda();
         vdeuda.setTitle("Registrar Deuda");
         vdeuda.setLocationRelativeTo(null);
         vdeuda.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        System.exit(0);
+        
+    }//GEN-LAST:event_btnExitActionPerformed
+
+    private void btnmenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnmenuActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnmenuActionPerformed
+
+    private void btnMinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMinActionPerformed
+        this.setExtendedState(ICONIFIED);
+    }//GEN-LAST:event_btnMinActionPerformed
     
 
     /**
@@ -425,6 +484,8 @@ Panel_Menu_Finanzas.setVisible(false);
     private javax.swing.JButton btn2;
     private javax.swing.JButton btn3;
     private javax.swing.JButton btn4;
+    private javax.swing.JButton btnExit;
+    private javax.swing.JButton btnMin;
     private javax.swing.JButton btnmenu;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
